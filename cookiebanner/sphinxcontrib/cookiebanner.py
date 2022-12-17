252273256241025c576555eee6bee293c1751775
Danny Diaz <texasdiaz@gmail.com>
2022-12-16 23:04:55 -0500

    Update cookiebanner.py

--- cookiebanner/sphinxcontrib/cookiebanner.py
+++ cookiebanner/sphinxcontrib/cookiebanner.py
@@ -7,14 +7,38 @@ from sphinx.errors import ExtensionError
 def add_cb_javascript(app, pagename, templatename, context, doctree):
     if not app.config.cookiebanner_enabled:
         return
-
-    metatags = context.get('metatags', '')
-    metatags += """
-    <!-- Start of HubSpot Embed Code MKT -->
-    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/1747660.js"></script>
-    <!-- End of HubSpot Embed Code MKT -->
+    
+    # Embed code into the header, using the "metatags" space.
+    headercode = """
+       <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
     """
-    context['metatags'] = metatags
+    context['metatags'] += headercode
+    
+    # Embed code into the body, using the "body" space.
+    bodycode = """
+       <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+       <script>
+          window.cookieconsent.initialise({
+             "palette": {
+                "popup": {
+                   "background": "#000000",
+                   "text": "#ffffff"
+                },
+                "button": {
+                   "background": "#ffffff",
+                   "text": "#000000"
+                }
+             },
+             "theme": "classic",
+             "position": "top",
+             "content": {
+                "message": "This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with the &lt;i&gt;FIRST&lt;/i&gt; Privacy Policy.",
+                "href": "https://www.firstinspires.org/about/privacy-policy"
+             }
+          });
+       </script>
+    """
+    context['body'] += bodycode
 
 def setup(app):
     app.add_config_value('cookiebanner_enabled', True, 'html')
