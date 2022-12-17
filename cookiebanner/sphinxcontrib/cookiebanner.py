e8f7b19664b562800bc7df39ffb838c7cab603eb
Danny Diaz <texasdiaz@gmail.com>
2022-12-16 23:28:43 -0500

    Update cookiebanner.py

--- cookiebanner/sphinxcontrib/cookiebanner.py
+++ cookiebanner/sphinxcontrib/cookiebanner.py
@@ -9,12 +9,14 @@ def add_cb_javascript(app, pagename, templatename, context, doctree):
         return
     
     # Embed code into the header, using the "metatags" space.
+    metatags_string = context.get('metatags','')
     headercode = """
        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
     """
-    context['metatags'] += headercode
+    context['metatags'] = metatags_string + headercode
     
     # Embed code into the body, using the "body" space.
+    body_string = context.get('body','')
     bodycode = """
        <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
        <script>
@@ -38,8 +40,9 @@ def add_cb_javascript(app, pagename, templatename, context, doctree):
           });
        </script>
     """
-    context['body'] += bodycode
+    context['body'] += body_string + bodycode
 
+    
 def setup(app):
     app.add_config_value('cookiebanner_enabled', True, 'html')
     app.connect('html-page-context', add_cb_javascript)
